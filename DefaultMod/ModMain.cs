f2e3df25667146826e3eff07e259babf71f37ee8
Moonaliss1 <60161840+Moonaliss1@users.noreply.github.com>
2020-04-24 14:13:36 -0500

    update to version 1.1

--- DefaultMod/ModMain.cs
+++ DefaultMod/ModMain.cs
@@ -23,18 +23,18 @@ namespace portalgoos
 {
     public class ModEntryPoint : IMod
     {
-        // Gets called automatically, passes in a class that contains pointers to
-
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(Keys vKey);
         Vector2 p1pos = new Vector2(300, 500);
         Vector2 p2pos = new Vector2(900, 500);
         Image p1img = Image.FromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "p1.png"));
         Image p2img = Image.FromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "p2.png"));
         bool justTeleported = false;
+        bool portalson = true;
+        bool p0pressed = false;
 
-        // useful functions we need to interface with the goose.
         void IMod.Init()
         {
-            // Subscribe to whatever events we want
             justTeleported = false;
             InjectionPoints.PostTickEvent += PostTick;
             InjectionPoints.PreRenderEvent += PreRender;
@@ -43,13 +43,16 @@ namespace portalgoos
 
         private void PreRender(GooseEntity goos, Graphics g)
         {
-            g.DrawImage(p1img, p1pos.x - p1img.Size.Width / 2, p1pos.y - p1img.Size.Height / 2);
-            g.DrawImage(p2img, p2pos.x - p2img.Size.Width / 2, p2pos.y - p2img.Size.Height / 2);
+            if (portalson)
+            {
+                g.DrawImage(p1img, p1pos.x - p1img.Size.Width / 2, p1pos.y - p1img.Size.Height / 2);
+                g.DrawImage(p2img, p2pos.x - p2img.Size.Width / 2, p2pos.y - p2img.Size.Height / 2);
+            }
         }
 
         public void PostTick(GooseEntity g)
         {
-            if (!justTeleported)
+            if (!justTeleported && portalson)
             {
                 if ((g.position.x > p1pos.x - p1img.Size.Width / 2) && (g.position.x < p1pos.x + p1img.Size.Width / 2) && (g.position.y > p1pos.y - p1img.Size.Height / 2) && (g.position.y < p1pos.y + p1img.Size.Height / 2))
                 {
@@ -69,28 +72,37 @@ namespace portalgoos
                 justTeleported = false;
             }
 
-            if (System.Windows.Input.Keyboard.IsKeyDown(System.Windows.Input.Key.P))
+            if ((GetAsyncKeyState((Keys)Enum.Parse(typeof(Keys), "p" , true)) != 0) )
             {
 
-                if (System.Windows.Input.Keyboard.IsKeyDown(System.Windows.Input.Key.D1))
+                if ((GetAsyncKeyState((Keys)Enum.Parse(typeof(Keys), "D1", true)) != 0))
                 {
                     p1pos = new Vector2(Input.mouseX, Input.mouseY);
-                }
-                if (System.Windows.Input.Keyboard.IsKeyDown(System.Windows.Input.Key.D2))
+                    }
+                if ((GetAsyncKeyState((Keys)Enum.Parse(typeof(Keys), "D2", true)) != 0))
                 {
                     p2pos = new Vector2(Input.mouseX, Input.mouseY);
+                }
+                if ((GetAsyncKeyState((Keys)Enum.Parse(typeof(Keys), "D0", true)) != 0) && !p0pressed)
+                {
+                    portalson = !portalson;
+                    p0pressed = true;
+                    System.Threading.Thread.Sleep(300);
+                    p0pressed = false;
                 }
 
             }
             
             // If we're running our mod's task
-            if (g.currentTask == API.TaskDatabase.getTaskIndexByID("Portal1"))
+            if (g.currentTask == API.TaskDatabase.getTaskIndexByID("Portal1") && portalson)
             {
                 g.targetPos = p1pos;
+                g.extendingNeck = true;
             }
-            if (g.currentTask == API.TaskDatabase.getTaskIndexByID("Portal2"))
+            if (g.currentTask == API.TaskDatabase.getTaskIndexByID("Portal2") && portalson)
             {
                 g.targetPos = p2pos;
+                g.extendingNeck = true;
             }
         }  
         
